8845cd5fb704fd8b208305fbfcda3e13ea6738e2
aird1990 <60633871+aird1990@users.noreply.github.com>
2021-01-03 17:59:59 +0900

    Update youtubeAudioDownloader.py

--- youtubeAudioDownloader.py
+++ youtubeAudioDownloader.py
@@ -1,8 +1,10 @@
-# You have to install "Python". And You have to install "Pytube".
+# This program downloads youtube audio/mp4 files. And convert mp4 to mp3.
+# You have to install "Python". And You have to install "Pytube"and "MoviePy".
 # url = ' '  　←YouTube URL
 # download_folder = ' '   ←Download folder path
 
-# 注意 事前にPythonをインストールする必要があります。またPytubeもインストールする必要があります。
+# このプログラムは、YouTubeのaudio/mp4ファイルをダウンロードします。そしてmp4をmp3に変換します。
+# 注意 事前にPythonをインストールする必要があります。またPytubeもmoviepyもインストールする必要があります。
 # urlのところにダウンロードしたい動画のurlを張ってください
 # download_folderのところにダウンロードしたいところのフォルダのパスを張ってください
 import pytube
@@ -10,3 +12,19 @@ url = 'https://www.youtube.com/watch?v=QDX-1GuF2Gs'
 download_folder = 'C:/Users/AIR-D/Desktop'
 
 format_list2 = pytube.YouTube(url).streams.filter(mime_type='audio/mp4').first().download(download_folder)
+
+import os
+
+os.chdir(download_folder)
+print(os.getcwd())
+
+import moviepy.editor as mp
+
+mp4_file = pytube.YouTube(url).title + '.mp4'
+mp3_file = pytube.YouTube(url).title + '.mp3'
+
+clip = mp.AudioFileClip(mp4_file)
+clip.write_audiofile(mp3_file)
+clip.close()
+
+os.remove(mp4_file)
